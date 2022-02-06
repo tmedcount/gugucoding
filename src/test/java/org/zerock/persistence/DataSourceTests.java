d703f73e6316ac1d1d328cf2b9fab216466a7798
yamto <yamto@118.42.187.153>
2022-02-07 04:03:35 +0900

    SQLSessionFactory

--- src/test/java/org/zerock/persistence/DataSourceTests.java
+++ src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +22,23 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class DataSourceTests {
 	
-	@Setter(onMethod_ = @Autowired)
+	@Setter(onMethod_ = {@Autowired})
 	private DataSource dataSource;
+	
+	@Setter(onMethod_ = @Autowired)
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testMyBatis() {
+		try(SqlSession session = sqlSessionFactory.openSession();
+			Connection con = session.getConnection();
+		) {
+			log.info(session);
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
 	
 	@Test
 	public void testConnection() {
